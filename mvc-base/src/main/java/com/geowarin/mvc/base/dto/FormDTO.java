d6652628cc693b8d7f1aa0d70bf9a939865e789d
dddlss-local <alassane.dia@forem.be>
2019-07-10 14:38:53 +0200

    test commit develop

--- mvc-base/src/main/java/com/geowarin/mvc/base/dto/FormDTO.java
+++ mvc-base/src/main/java/com/geowarin/mvc/base/dto/FormDTO.java
@@ -8,6 +8,7 @@ public class FormDTO {
 	private String messageFromUser;
 /**
  * return the message from user
+ * a other comment test for develop branch
  * @return
  */
 	public String getMessageFromUser() {
