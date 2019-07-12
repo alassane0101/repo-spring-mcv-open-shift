7670bfd2ebb2f38e5fdbd762e55274b4f3116e24
MBEKE N'SAKA Pepe <pepe.mbekensaka@forem.be>
2019-07-12 10:44:33 +0200

    Customer class created by pépé.

---
+++ mvc-base/src/main/java/com/geowarin/mvc/base/dto/Customer.java
@@ -0,0 +1,43 @@
+package com.geowarin.mvc.base.dto;
+
+public class Customer 
+{
+   private String firstname;
+   private String lastname;
+   private String city;
+   
+   public Customer()
+   {
+	   
+   }
+
+   public String getFirstname()  
+   {
+	  return firstname;
+   }
+
+   public void setFirstname(String firstname) 
+   {
+	  this.firstname = firstname;
+   }
+
+   public String getLastname() 
+   {
+	  return lastname;
+   }
+
+   public void setLastname(String lastname) 
+   {
+	  this.lastname = lastname;
+   }
+
+   public String getCity() 
+   {
+	  return city;
+   }
+
+   public void setCity(String city) 
+   {
+	  this.city = city;
+   }
+}
